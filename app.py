97c9b7003f9ef2d6394af9f03418772af73bf171
Isaac Watkins <iwatkins10453@gmail.com>
2021-05-12 08:02:57 -0400

    altered route var to take query | added login routes

--- app.py
+++ app.py
@@ -3,12 +3,48 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# return a name backwards
+@app.route('/user/<username>')
+def say_hello_var(username):
+    greet = F'Hi, {username}'
+    return greet[::-1]
+
+
+# return a number squared
+@app.route('/num/')
+def square(number):
+    return str(number * number)
+
+# GET, reqs
+# return a name n number of times
+@app.route('/numname/<int:num>/<name>')
+def numnamer(num,name):
+    return str(name * 5)
+
+@app.route('/hello', methods=['GET']) #GET is the default method
+def say_hello():
+    return "Hello, World!"
+    return """
+    <body style="background-color='black'">
+        <h1>fifth of w</h1>
+    </body>
+    """
+# return a number doubled
+@app.route('/double')
+def double():
+    number = request.args.get('number')
+    number = int(number)
+    return str(number * 2)
+    
+# search action route
 @app.route('/search')
 def search_page():
     name = request.args.get('name')
     category = request.args.get('category')
+    num = request.args.get('num')
     return f"You searched for {name} in the category {category}"
 
+# search form route
 @app.route('/search_form')
 def search_form():
     return """
@@ -21,35 +57,26 @@ def search_form():
     </form>
     """
 
-@app.route('/hello')
-def say_hello():
-    return "Hello, World!"
 
-# return a name backwards
-@app.route('/user/<username>')
-def say_hello_var(username):
-    greet = F'Hi, {username}'
-    return greet[::-1]
+# login GET REQ
+@app.route('/login')
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return f"username is {username} & password is {password}"
 
-# return a number doubled
-@app.route('/double/<int:the_number>')
-def double(the_number):
-    return str(the_number * 2)
 
-# return a number squared
-@app.route('/num/<int:number>')
-def square(number):
-    return str(number * number)
-
-# return a name n number of times
-@app.route('/numname/<int:num>/<name>')
-def numnamer(num,name):
-    return str(name * 5)
-
-@app.route('/main')
-def mainer():   
+# POST REQ LOGIN FORM
+@app.route('/login_form')
+def login_form():
     return """
-    <body style="background-color='black'">
-        <h1>fifth of w</h1>
-    </body>
+    <form action='/login' method='POST'>
+        Username: <input type='text' name='username'>
+        <br>
+        Password: <input type='password' name='password'> 
+        <br>
+        <input type='submit'>
+    </form>
     """
+
+
